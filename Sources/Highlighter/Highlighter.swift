7567de1dcf0b53ab8a3945720a2cff873c9416f2
Leon Li <arkadeleon@gmail.com>
2020-10-12 13:53:53 +0800

    Implement basic function

--- Sources/Highlighter/Highlighter.swift
+++ Sources/Highlighter/Highlighter.swift
@@ -1,3 +1,51 @@
-struct Highlighter {
-    var text = "Hello, World!"
+
+import UIKit
+import JavaScriptCore
+
+open class Highlighter {
+
+    private let hljs: JSValue
+
+    public init?() {
+        let context = JSContext()!
+
+        let jsURL = Bundle.module.bundleURL.appendingPathComponent("Highlight.js/highlight.pack.js")
+        let jsContents = try! String(contentsOf: jsURL)
+        context.evaluateScript(jsContents)
+
+        guard let hljs = context.objectForKeyedSubscript("hljs") else {
+            return nil
+        }
+
+        self.hljs = hljs
+    }
+
+    open func highlight(_ code: String) -> NSAttributedString? {
+        let cssURL = Bundle.module.bundleURL.appendingPathComponent("Highlight.js/styles/default.css")
+        let cssContents = try! String(contentsOf: cssURL)
+
+        guard let highlightedCode = hljs
+                .invokeMethod("highlightAuto", withArguments: [code])?.objectForKeyedSubscript("value")?.toString() else {
+            return nil
+        }
+
+        let string = """
+        <style>
+            \(cssContents)
+        </style>
+        <pre><code class="hljs">
+            \(highlightedCode)
+        </code></pre>
+        """
+        guard let data = string.data(using: .utf8) else {
+            return nil
+        }
+
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+
+        return try? NSAttributedString(data: data, options: options, documentAttributes: nil)
+    }
 }
--- Tests/HighlighterTests/HighlighterTests.swift
+++ Tests/HighlighterTests/HighlighterTests.swift
@@ -6,7 +6,18 @@ final class HighlighterTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Highlighter().text, "Hello, World!")
+        let highlighter = Highlighter()!
+        let code = """
+        struct Highlighter {
+            var text = "Hello, World!"
+        }
+        """
+        let highlightedCode = highlighter.highlight(code)
+        let renderer = UIGraphicsImageRenderer(size: highlightedCode?.size() ?? .zero)
+        let image = renderer.image { (context) in
+            highlightedCode?.draw(at: .zero)
+        }
+        XCTAssertEqual(image.size, CGSize(width: 216.5, height: 60))
     }
 
     static var allTests = [
