8dd3f3453363caed816b64cbc7980d2b7313ea4b
Leon Li <arkadeleon@gmail.com>
2021-06-22 13:20:02 +0800

    Update highlight.js to 11.0.1

--- Highlight/WKWebView+Highlight.swift
+++ Highlight/WKWebView+Highlight.swift
@@ -6,37 +6,22 @@
 //
 
 import WebKit
-import JavaScriptCore
 
 extension WKWebView {
 
     @discardableResult
     public func loadCode(_ code: String, style: HighlightStyle = .default) -> WKNavigation? {
-        guard let context = JSContext() else {
-            return nil
-        }
-
         let bundle = Bundle(identifier: "date.leonandvane.highlight")!
         let baseURL = bundle.resourceURL!.appendingPathComponent("Highlight.js")
-
-        let jsURL = baseURL.appendingPathComponent("highlight.pack.js")
-        let jsContents = try! String(contentsOf: jsURL)
-        context.evaluateScript(jsContents)
-
-        guard let hljs = context.objectForKeyedSubscript("hljs") else {
-            return nil
-        }
-
-        guard let highlightedCode = hljs.invokeMethod("highlightAuto", withArguments: [code])?.objectForKeyedSubscript("value")?.toString() else {
-            return nil
-        }
 
         let html = """
         <!doctype html>
         <meta charset="utf-8">
         <meta name="viewport" content="height=device-height, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-        <link rel="stylesheet" href="styles/\(style.rawValue).css">
-        <pre><code class="hljs">\(highlightedCode)</code></pre>
+        <link rel="stylesheet" href="styles/\(style.rawValue).min.css">
+        <script src="highlight.min.js"></script>
+        <script>hljs.highlightAll();</script>
+        <pre><code>\(code)</code></pre>
         """
 
         return loadHTMLString(html, baseURL: baseURL)
