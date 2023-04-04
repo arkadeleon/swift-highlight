09402e27bdeedb6b3db66beb8e28e0b52b36e1c1
Leon Li <arkadeleon@gmail.com>
2023-04-04 17:45:41 +0800

    Add line numbers

--- Sources/Highlight/WKWebView+Highlight.swift
+++ Sources/Highlight/WKWebView+Highlight.swift
@@ -19,7 +19,34 @@ extension WKWebView {
         <meta name="viewport" content="height=device-height, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
         <link rel="stylesheet" href="styles/\(style.rawValue).min.css">
         <script src="highlight.min.js"></script>
-        <script>hljs.highlightAll();</script>
+        <script src="highlightjs-line-numbers.min.js"></script>
+        <script>
+            hljs.highlightAll();
+            hljs.initLineNumbersOnLoad();
+        </script>
+        <style>
+            td.hljs-ln-numbers {
+                -webkit-touch-callout: none;
+                -webkit-user-select: none;
+                -khtml-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+
+                text-align: center;
+                color: #ccc;
+                border-right: 1px solid #ccc;
+                vertical-align: top;
+                padding-right: 5px;
+            }
+            td.hljs-ln-code {
+                padding-left: 10px;
+            }
+            code {
+                white-space: pre-wrap;
+                overflow: auto;
+            }
+        </style>
         <pre><code>\(code)</code></pre>
         """
 
